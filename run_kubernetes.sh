541c0459b410fc72e0a3d08d2be9bedb85bdb2ab
Rowena Foo <rwnfoo@github.com>
2020-07-21 19:04:44 -0500

    edit commands to run kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=rwnfoo/housing:ver.1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run housing-predictor\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80\
+    --labels app=housing
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward housing-predictor 8000:80
